a40a04e5fc1145d1a0585b3bb720a18ffe82a124
vaalar <hugo01frege@gmail.com>
2023-03-01 19:51:15 +0100

    Added two new versions o the notebook.

--- Clustering/tests.py
+++ Clustering/tests.py
@@ -1,16 +1,8 @@
 import threading as th
 import time
 
-def print_time( threadName, delay):
-    count = 0
-    time.sleep(delay)
-    return delay
-try:
-    number = 
-    number2 = _thread.start_new_thread( print_time, ("Thread-2", 4, ) )
-    print(number)
-    print(number2)
-except:
-   print ("Error: unable to start thread")
-while 1:
-   pass
+def positive(x):
+    return x <= 0
+
+print(min(filter(lambda x : x >= 0, [-1, 2, 0.0, 1])))
+
